e6b867a6b00f2761a580d461601ae1999344e40b
Maria Let <124286208+marialet14@users.noreply.github.com>
2025-06-30 13:32:49 -0300

    Update Exportar.java

--- src/main/java/br/upe/business/Exportar.java
+++ src/main/java/br/upe/business/Exportar.java
@@ -9,11 +9,11 @@ public class IndicadoresService {
 
     public void exportarParaCSV(List<Indicadores> indicadoresList, String caminhoArquivo) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
-            // Escrever cabeçalho
+            
             writer.write("Data,Peso,Altura,PorcentagemGordura,PorcentagemMM,IMC");
             writer.newLine();
 
-            // Escrever dados
+          
             for (Indicadores indicador : indicadoresList) {
                 writer.write(String.format("%s,%.2f,%.2f,%.2f,%.2f,%.2f",
                         indicador.getData(),
