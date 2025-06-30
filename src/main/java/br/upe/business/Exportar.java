6d257e7d7a39e8c6d1b04b3711ec40ca26f91889
Maria Let <124286208+marialet14@users.noreply.github.com>
2025-06-30 13:31:49 -0300

    Create Exportar.java

---
+++ src/main/java/br/upe/business/Exportar.java
@@ -0,0 +1,31 @@
+package upe.br.business
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class IndicadoresService {
+
+    public void exportarParaCSV(List<Indicadores> indicadoresList, String caminhoArquivo) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(caminhoArquivo))) {
+            // Escrever cabeçalho
+            writer.write("Data,Peso,Altura,PorcentagemGordura,PorcentagemMM,IMC");
+            writer.newLine();
+
+            // Escrever dados
+            for (Indicadores indicador : indicadoresList) {
+                writer.write(String.format("%s,%.2f,%.2f,%.2f,%.2f,%.2f",
+                        indicador.getData(),
+                        indicador.getPeso(),
+                        indicador.getAltura(),
+                        indicador.getPorcentagemGordura(),
+                        indicador.getPorcentagemMM(),
+                        indicador.getImc()));
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
